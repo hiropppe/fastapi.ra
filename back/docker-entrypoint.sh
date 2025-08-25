a9a00c21d451cfa75c64c1b3c3155fb9d70ee15b
hiropppe <sergei.0910@gmail.com>
2025-08-25 17:07:38 +0900

    map host user to container user at entrypoint

--- back/docker-entrypoint.sh
+++ back/docker-entrypoint.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-exec "$@"
+
+export USER=take
+export HOME=/home/$USER
+
+uid=$(stat -c "%u" .)
+gid=$(stat -c "%g" .)
+
+if [ "$(id -u)" -eq 0 ]; then
+  if [ "$uid" -ne 0 ]; then
+    if [ "$(id -g $USER)" -ne $gid ]; then
+        getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
+        chgrp -R $gid $HOME
+    fi
+    if [ "$(id -u $USER)" -ne $uid ]; then
+        usermod -u $uid $USER
+    fi
+  fi
+  chown -R $USER:$USER /app
+  exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
+else
+  exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
+  #exec "$@"
+fi
+
