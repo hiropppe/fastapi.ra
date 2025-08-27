f1dc46dc212653ce73bf90030f9b9969728c1134
hiropppe <sergei.0910@gmail.com>
2025-08-27 17:13:53 +0900

    update

--- back/docker-entrypoint.sh
+++ back/docker-entrypoint.sh
@@ -6,13 +6,13 @@ export HOME=/home/$USER
 uid=$(stat -c "%u" .)
 gid=$(stat -c "%g" .)
 
-if [ "$uid" -ne 0 ]; then
+if [ "$(id -u)" -eq 0 ] && [ "$uid" -ne 0 ]; then
   if [ "$(id -g $USER)" -ne $gid ]; then
-      sudo getent group $gid >/dev/null 2>&1 || sudo groupmod -g $gid $USER
-      sudo chgrp -R $gid $HOME
+      getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
+      chgrp -R $gid $HOME
   fi
   if [ "$(id -u $USER)" -ne $uid ]; then
-      sudo usermod -u $uid $USER
+      usermod -u $uid $USER
   fi
 fi
 
