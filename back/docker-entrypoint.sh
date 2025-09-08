124090867f9ff2c363dfb52ea6d027cc61fe4581
hiropppe <sergei.0910@gmail.com>
2025-09-08 17:47:31 +0900

    update

--- back/docker-entrypoint.sh
+++ back/docker-entrypoint.sh
@@ -6,19 +6,27 @@ export HOME=/home/$USER
 uid=$(stat -c "%u" .)
 gid=$(stat -c "%g" .)
 
-if [ "$(id -u)" -eq 0 ] && [ "$uid" -ne 0 ]; then
+if [ "$BUILD_MODE" == "devel" ]; then
+  if [ "$(id -u)" -eq 0 ]; then
+    sd=""  
+  else
+    sd="sudo"
+  fi
   if [ "$(id -g $USER)" -ne $gid ]; then
-      getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
-      chgrp -R $gid $HOME
+    $sd getent group $gid >/dev/null 2>&1 || $sd groupmod -g $gid $USER
+    $sd chgrp -R $gid $HOME
   fi
   if [ "$(id -u $USER)" -ne $uid ]; then
-      usermod -u $uid $USER
+    $sd usermod -u $uid $USER
   fi
 fi
 
 if [ "$(id -u)" -eq 0 ]; then
   exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
 else
+  ## TODO. error after changing uid and gid.
+  # groups: cannot find name for group ID 1000
+  # bash: /home/take/.bashrc: Permission denied
   exec "$@"
 fi
 
