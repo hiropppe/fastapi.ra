b98f88d79aaf22c011dc53b5ae4973c6b44cbddf
hiropppe <sergei.0910@gmail.com>
2025-09-09 10:07:02 +0900

    Maybe I'll just always use root then

--- back/docker-entrypoint.sh
+++ back/docker-entrypoint.sh
@@ -6,27 +6,39 @@ export HOME=/home/$USER
 uid=$(stat -c "%u" .)
 gid=$(stat -c "%g" .)
 
-if [ "$BUILD_MODE" == "devel" ]; then
-  if [ "$(id -u)" -eq 0 ]; then
-    sd=""  
-  else
-    sd="sudo"
-  fi
+if [ "$uid" -ne 0 ]; then
   if [ "$(id -g $USER)" -ne $gid ]; then
-    $sd getent group $gid >/dev/null 2>&1 || $sd groupmod -g $gid $USER
-    $sd chgrp -R $gid $HOME
+    getent group $gid >/dev/null 2>&1 || groupmod -g $gid $USER
+    chgrp -R $gid $HOME
   fi
   if [ "$(id -u $USER)" -ne $uid ]; then
-    $sd usermod -u $uid $USER
+    usermod -u $uid $USER
   fi
 fi
 
-if [ "$(id -u)" -eq 0 ]; then
-  exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
-else
-  ## TODO. error after changing uid and gid.
-  # groups: cannot find name for group ID 1000
-  # bash: /home/take/.bashrc: Permission denied
-  exec "$@"
-fi
+exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
+
+#if [ "$BUILD_MODE" == "devel" ]; then
+#  if [ "$(id -u)" -eq 0 ]; then
+#    sd=""  
+#  else
+#    sd="sudo"
+#  fi
+#  if [ "$(id -g $USER)" -ne $gid ]; then
+#    $sd getent group $gid >/dev/null 2>&1 || $sd groupmod -g $gid $USER
+#    $sd chgrp -R $gid $HOME
+#  fi
+#  if [ "$(id -u $USER)" -ne $uid ]; then
+#    $sd usermod -u $uid $USER
+#  fi
+#fi
+
+#if [ "$(id -u)" -eq 0 ]; then
+#  exec setpriv --reuid=$USER --regid=$USER --init-groups "$@"
+#else
+#  ## Error after changing uid and gid when execcuted by a non-root user.
+#  # groups: cannot find name for group ID 1000
+#  # bash: /home/take/.bashrc: Permission denied
+#  exec "$@"
+#fi
 
