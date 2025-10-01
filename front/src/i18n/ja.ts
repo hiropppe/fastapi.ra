dbeb7a14a794781667d68ff053740490eee08b5e
hiropppe <sergei.0910@gmail.com>
2025-10-01 15:52:05 +0900

    update

--- front/src/i18n/ja.ts
+++ front/src/i18n/ja.ts
@@ -22,6 +22,38 @@ const customJapaneseMessages: TranslationMessages = {
             }
         },
     },
+    auth: {
+        sign_in: 'ログイン',
+        username: 'ユーザ名',
+        username_or_email: 'ユーザ名またはメールアドレス',
+        password: 'パスワード',
+        confirm_password: 'パスワード（確認）',
+        old_password: '現在のパスワード',
+        new_password: '新しいパスワード',
+        confirm_new_password: '新しいパスワード（確認）',
+        cognito: {
+            mfa: {
+                totp: {
+                    message: '登録されているメールアドレスに送信された認証コードをご入力ください。',
+                    label: '認証コード'
+                }
+            }
+        },
+        require_new_password: '新しいパスワードを入力してください。',
+        password_policy: {
+            available_chars: '半角英数字および半角記号 ^ $ * . [ ] { } ( ) ? - " ! @ # % & / \ , > < \' : ; | _ ~ ` + = を使用 ',
+            length8: '8文字以上',
+            max_char_vars: '数字、記号、大文字、小文字をそれぞれ1文字以上含む',
+            contains_number: '数字を1文字以上含む',
+            contains_special: '記号を1文字以上含む',
+            contains_lowercase: '小文字を1文字以上含む',
+            contains_uppercase: '大文字を1文字以上含む',
+        },
+        errors: {
+            auth_session_expired: '認証セッション切れです、最初からやり直してください',
+            session_expired: 'セッション切れです、再度ログインしてください'
+        }
+    },
     resources: {
         users: {
             name: 'User |||| Users',
